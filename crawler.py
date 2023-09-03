76e407e4e45fbbbcd3b0a87f95e9de58c4e74c9e
Amanda Pearce <amandagpearce@gmail.com>
2023-09-03 10:24:05 -0300

    retrieve image from google images

---
+++ crawler.py
@@ -0,0 +1,29 @@
+from google_images_search import GoogleImagesSearch
+
+
+def get_artwork_imageUrl(query):
+    # params are
+    # Googles programmable search engine API key
+    # Search Engine ID from google's programmable search engine controlpanel
+    gis = GoogleImagesSearch(
+    )
+
+    search_params = {
+        "q": query,
+        "num": 1,  # Number of images to fetch
+        "safe": "high",  # Safety level (options: high, medium, off)
+        "fileType": "jpg|png",  # Limit search to JPEG and PNG files
+        "size": "large",  # Filter for large images
+        "rights": (  # Creative Commons licenses
+            "cc_publicdomain|"
+            "cc_attribute|"
+            "cc_sharealike|"
+            "cc_noncommercial|"
+            "cc_nonderived"
+        ),
+    }
+
+    gis.search(search_params=search_params)
+    result = gis.results()[0]  # Get the first image result
+
+    return result.url if result else None
