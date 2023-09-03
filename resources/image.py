afa072e9e98350cf3667168444ac3716f11737f6
Amanda Pearce <amandagpearce@gmail.com>
2023-09-03 19:36:34 -0300

    include artwork artist in the image search

--- resources/image.py
+++ resources/image.py
@@ -6,14 +6,14 @@ from flask.views import MethodView
 from db import db
 from schemas import ArtworkImageSchema
 from models import ArtworkImageModel
-from crawler import get_artwork_imageUrl
+from crawler import get_artwork_image_url
 
 blp = Blueprint(
     "Get Image URL", __name__, description="Operations with Google Images"
 )
 
 
-@blp.route("/get_imageUrl")
+@blp.route("/get_image_url")
 class ImageUrl(MethodView):
     @blp.response(200, ArtworkImageSchema)
     def get(self):
@@ -22,12 +22,13 @@ class ImageUrl(MethodView):
         # gets the query params
         artwork_id = request.args.get("artworkId")
         artwork_title = request.args.get("artworkTitle")
+        artwork_artist = request.args.get("artist")
 
-        print("artwork_id")
-        print(artwork_id)
+        # print("artwork_id")
+        # print(artwork_id)
 
-        print("artwork_title")
-        print(artwork_title)
+        # print("artwork_title")
+        # print(artwork_title)
 
         if artwork_id and artwork_title:
             artworkRecord = ArtworkImageModel.query.get(artwork_id)
@@ -35,25 +36,27 @@ class ImageUrl(MethodView):
             if artworkRecord:
                 artworkUrl = artworkRecord.imageUrl
             else:
-                artworkUrl = self.get_imageUrl(artwork_id, artwork_title)
+                artworkUrl = self.get_imageUrl(
+                    artwork_id, artwork_title, artwork_artist
+                )
+
+                print("artworkUrl")
+                print(artworkUrl)
 
             return artworkUrl
         else:
             abort(400, message="Missing artworkId or artworkTitle parameter.")
 
-    def get_imageUrl(self, artwork_id, artwork_title):
-        imageUrl = get_artwork_imageUrl(artwork_title)
-
-        print("#")
-        print(artwork_id, artwork_title)
-        print("#")
+    def get_imageUrl(self, artwork_id, artwork_title, artwork_artist):
+        imageUrl = get_artwork_image_url(f"{artwork_title} {artwork_artist}")
 
         if imageUrl:
             artwork_image = ArtworkImageModel(
-                artwork_id=artwork_id, imageUrl=imageUrl
+                artworkId=artwork_id, imageUrl=imageUrl, artist=artwork_artist
             )
             db.session.add(artwork_image)
             db.session.commit()
+
             return artwork_image
         else:
             return None
