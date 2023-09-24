6f87936834567573e70eadc8458f5eec23acca2e
Amanda Pearce <amandagpearce@gmail.com>
2023-09-24 12:19:36 -0300

    artworkImage model update

--- models/artworkImage.py
+++ models/artworkImage.py
@@ -3,6 +3,6 @@ from db import db
 
 class ArtworkImageModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    artworkId = db.Column(db.Integer, nullable=False)
+    artworkTitle = db.Column(db.String, nullable=False)
     imageUrl = db.Column(db.String, nullable=False)
     artist = db.Column(db.String, nullable=False)
